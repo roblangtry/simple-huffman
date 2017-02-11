329353d439a4c4e96644a9211f86d6fc9ac05846
Robert Langtry <rob.langtry2@gmail.com>
2017-02-11 23:59:10 +1100

    Add double include guard defines to header files

--- src/compress.h
+++ src/compress.h
@@ -1,2 +1,5 @@
 #include <stdio.h>
+#ifndef COMPRESSION_CODE
+#define COMPRESSION_CODE
 int run_compression(char *input_filename, char *output_filename, int verbose_flag);
+#endif
--- src/decompress.h
+++ src/decompress.h
@@ -1,2 +1,5 @@
 #include <stdio.h>
+#ifndef DECOMPRESSION_CODE
+#define DECOMPRESSION_CODE
 int run_decompression(char *input_filename, char *output_filename, int verbose_flag);
+#endif
