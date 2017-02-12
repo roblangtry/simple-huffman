b01513ad944be935e796efe9f070e9161c3ef610
Robert Langtry <rob.langtry2@gmail.com>
2017-02-12 20:58:05 +1100

    Working commit adding functional symbol unnormalised probability evaluation

--- src/compress.c
+++ src/compress.c
@@ -3,12 +3,62 @@ int run_compression(char *input_filename, char *output_filename, int verbose_fla
     FILE *input_file_pointer;
     FILE *output_file_pointer;
     int value;
+    struct probability_list list;
     input_file_pointer = fopen(input_filename, "r");
     output_file_pointer = fopen(output_filename, "w");
     while (fscanf(input_file_pointer, "%d\n", &value) != EOF) {
         fprintf(output_file_pointer, "%d\n", value);
     }
+
+    list = evaluate_symbol_probabilities(input_file_pointer);
     fclose(input_file_pointer);
     fclose(output_file_pointer);
     return 1;
+}
+struct probability_list evaluate_symbol_probabilities(FILE * input_file_pointer){
+    int temp_value;
+    struct probability_list list;
+    rewind(input_file_pointer);
+    list = initialise_probabilities_list(input_file_pointer);
+    while (fscanf(input_file_pointer, "%d\n", &temp_value) != EOF) {
+        add_to_probability_list(&list, temp_value);
+    }
+    return list;
+}
+struct probability_list initialise_probabilities_list(FILE * input_file_pointer){
+    int value;
+    fscanf(input_file_pointer, "%d\n", &value);
+    struct probability_list list;
+    struct probability_point point;
+    point.value = value;
+    point.occurrences = 1;
+    list.list_length = 1;
+    list.list = malloc(sizeof(struct probability_point));
+    list.list[0] = point;
+    return list;
+}
+void add_to_probability_list(struct probability_list * list, int value){
+    int i;
+    int list_len;
+    int added;
+    struct probability_point point;
+    list_len = (*list).list_length;
+    i = 0;
+    added = 0;
+    while (i < list_len && added == 0){
+        point = (*list).list[i];
+        if(point.value == value){
+            point.occurrences = point.occurrences + 1;
+            added = 1;
+            (*list).list[i] = point;
+        }
+        i++;
+    }
+    if(added == 0){
+        (*list).list_length = (*list).list_length + 1;
+        (*list).list = realloc((*list).list, sizeof(struct probability_point) * (*list).list_length);
+        point.value = value;
+        point.occurrences = 1;
+        (*list).list[(*list).list_length - 1] = point;
+    }
 }
--- src/compress.h
+++ src/compress.h
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #ifndef COMPRESSION_CODE
 #define COMPRESSION_CODE
+struct probability_point {
+    int value;
+    int occurrences;
+};
+struct probability_list {
+    int list_length;
+    struct probability_point * list;
+};
 int run_compression(char *input_filename, char *output_filename, int verbose_flag);
+struct probability_list evaluate_symbol_probabilities(FILE * input_file);
+struct probability_list initialise_probabilities_list(FILE * input_file);
+void add_to_probability_list(struct probability_list * list, int value);
 #endif
